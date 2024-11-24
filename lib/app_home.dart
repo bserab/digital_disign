8b0ec8b602a32b39912ace013d08cd6622df67d1
tai0312 <145961519+tai0312@users.noreply.github.com>
2024-11-24 21:16:32 +0900

    フッター追加

--- lib/app_home.dart
+++ lib/app_home.dart
@@ -2,8 +2,31 @@ import 'package:flutter/material.dart';
 import 'campus_tour_home.dart';
 
 //ホーム画面
-class AppHome extends StatelessWidget {
+class AppHome extends StatefulWidget {
   const AppHome({super.key});
+
+  @override
+  _AppHomeState createState() => _AppHomeState();
+}
+
+class _AppHomeState extends State<AppHome> {
+  int _selectedIndex = 0; // ナビゲーションバーの選択状態
+
+  void _onItemTapped(int index) {
+    if (index == 0) {
+      // 現在の画面が「マップ」なら何もしない
+      setState(() {
+        _selectedIndex = index;
+      });
+    } else if (index == 1) {
+      // キャンパスツアー画面に遷移
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const CampusTourHome()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,19 +35,21 @@ class AppHome extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // 子ページ（キャンパスツアー選択ページ）への遷移
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const CampusTourHome()),
-            );
-          },
-          style: ElevatedButton.styleFrom(
-            minimumSize: const Size(200, 50),
+        //ここに表示したいものを書く
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: "マップ",
           ),
-          child: const Text("キャンパスツアーを見る"),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flag),
+            label: "キャンパスツアー",
+          ),
+        ],
       ),
     );
   }
