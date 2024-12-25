e0316154788309bc75913ac2f2c164be18defdd0
tai0312 <145961519+tai0312@users.noreply.github.com>
2024-12-25 11:56:48 +0900

    ツアーのリスト表示

--- lib/tour_macker.dart
+++ lib/tour_macker.dart
@@ -35,6 +35,7 @@ class _TourMaker extends State<TourMaker> {
   Set<Polyline> _polylines = {};
 
   int _selectedIndex = 1; // BottomNavigationBarの選択状態
+  bool _isListVisible = false; // リストの表示状態
 
   // 現在地用
   late LocationData _currentLocation;
@@ -180,7 +181,6 @@ class _TourMaker extends State<TourMaker> {
     super.dispose();
   }
   void _onNextButtonPressed() {
-    
     setState(() {
       tourNum = (tourNum + 1) % widget.courseList.length; // 次の目的地に移動
     });
@@ -188,6 +188,12 @@ class _TourMaker extends State<TourMaker> {
       tourPosition.elementAt(tourNum)
     ));
     print(widget.courseList[tourNum]);
+  }
+
+  void _toggleListVisibility() {
+    setState(() {
+      _isListVisible = !_isListVisible;
+    });
   }
 
   @override
@@ -202,8 +208,15 @@ class _TourMaker extends State<TourMaker> {
         ),
         centerTitle: true,
         backgroundColor: const Color.fromRGBO(0, 98, 83, 1), // AppBarの背景色を変更
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.menu),
+            color: Colors.white,
+            onPressed: _toggleListVisibility, // メニューアイコンタップでリスト表示
+          ),
+        ],
       ),
-      body: Column(
+      body: Stack(
         children: [
           // 少し下に移動させるための余白を追加
           Container(
@@ -228,56 +241,129 @@ class _TourMaker extends State<TourMaker> {
               ),
             ),
           ),
-          Expanded(
-            child: GoogleMap(
-              onMapCreated: _onMapCreated,
-              mapType: MapType.hybrid,
-              markers: _markers..addAll(_currentLocationMarker != null ? {_currentLocationMarker!} : {}),
-              polylines: _polylines..addAll(_polylines),
-              initialCameraPosition: CameraPosition(
-                target: _markers.first.position,
-                zoom: 19.0,
-                bearing: 180,
-              ),
-            ),
-          ),
-          Container(
-          padding: const EdgeInsets.symmetric(vertical: 3.0, horizontal: 16.0),
-          color: Colors.white,
-          child: Row(
+          
+          Column(
             children: [
-              if (tourNum != 0)
-              SizedBox(
-                width: 70, // ボタンの横幅を固定
-                child: Text(
-                  '${tourNum == 0 ? "" : "目的地:"}',
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
               Expanded(
-                child: Text(
-                  '${widget.courseList[tourNum]}',
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+                child: GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  mapType: MapType.hybrid,
+                  markers: _markers..addAll(_currentLocationMarker != null ? {_currentLocationMarker!} : {}),
+                  polylines: _polylines..addAll(_polylines),
+                  initialCameraPosition: CameraPosition(
+                    target: _markers.first.position,
+                    zoom: 19.0,
+                    bearing: 180,
                   ),
                 ),
               ),
-              SizedBox(
-                width: 100, // ボタンの横幅を固定
-                child: RoundedButton(
-                  label: '次へ',
-                  onPressed: _onNextButtonPressed,
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 3.0, horizontal: 16.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    if (tourNum != 0)
+                    SizedBox(
+                      width: 70, // ボタンの横幅を固定
+                      child: Text(
+                        '${tourNum == 0 ? "" : "目的地:"}',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        '${widget.courseList[tourNum]}',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 100, // ボタンの横幅を固定
+                      child: RoundedButton(
+                        label: '次へ',
+                        onPressed: _onNextButtonPressed,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
+          // 半透明の背景
+        if (_isListVisible)
+          GestureDetector(
+            onTap: _toggleListVisibility, // 背景タップでリストを閉じる
+            child: Container(
+              color: Colors.black54, // 半透明の背景
+            ),
+          ),
+        // 右からスライドするリスト
+        AnimatedPositioned(
+          duration: const Duration(milliseconds: 300), // アニメーションの速度
+          top: 0,
+          bottom: 0,
+          right: _isListVisible ? 0 : -MediaQuery.of(context).size.width * 0.9, // 表示/非表示の位置
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.9, // 幅を画面の80%に設定
+            color: Colors.white,
+            child: Column(
+              children: [
+                // ヘッダー部分（閉じるボタン）
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        '案内順',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.close),
+                      onPressed: _toggleListVisibility, // リストを閉じる
+                    ),
+                  ],
+                ),
+                // リスト部分
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: widget.courseList.length,
+                    itemBuilder: (context, index) {
+                      final isCurrent = index == tourNum;
+                      return Container(
+                        color: isCurrent
+                            ? const Color.fromRGBO(166, 202, 236, 1) // 現在の地点の背景色
+                            : Colors.transparent, // デフォルトの背景色
+                        child: ListTile(
+                          title: Text(
+                            '${index + 1}. ${widget.courseList[index]}',
+                            style: TextStyle(
+                              fontWeight: isCurrent ? FontWeight.bold : FontWeight.normal,
+                              color: isCurrent ? Colors.black : Colors.grey[800],
+                            ),
+                          ),
+                          //onTap: () => _onListItemTap(index), // タップ処理
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
+          
         ],
       ),
       bottomNavigationBar: Column(
